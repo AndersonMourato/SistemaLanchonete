6dec18924f27a6f2cde2d17ebcc1088fc616cb1b
Fernanda Silva <cassia.messias.dev@gmail.com>
2024-05-25 15:50:46 -0300

    Teste1

--- SistemaLanchonete/src/Main.java
+++ SistemaLanchonete/src/Main.java
@@ -4,5 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Fernanda");
+
+        System.out.println("Fernanda");
+        System.out.println("Fernanda");
+
+
+
+
+
+
+
+
     }
 }
