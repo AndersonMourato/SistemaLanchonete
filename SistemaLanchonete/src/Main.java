654aa23868b673d747ef6f200d7fddca925cd724
Fernanda Silva <cassia.messias.dev@gmail.com>
2024-05-25 15:38:11 -0300

    Teste

--- SistemaLanchonete/src/Main.java
+++ SistemaLanchonete/src/Main.java
@@ -3,6 +3,6 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("FernandaSilvaMessias");
+        System.out.println("Fernanda");
     }
 }
