9038f08c0c352477fca7008741fcbc071b6fc3b9
Anderson Inacio <anderson.mouratoo@gmail.com>
2024-06-01 13:24:44 -0300

    save

--- SistemaLanchonete/src/Main.java
+++ SistemaLanchonete/src/Main.java
@@ -3,12 +3,6 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Fernanda");
-
-        System.out.println("Fernanda");
-        System.out.println("Fernanda");
-        System.out.println("Fernanda");
-
         System.out.println("Teste");
     }
 }
