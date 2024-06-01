58e2f4e42d48dfed632f78f81d7b02fafbb13e77
DeveloperTst <anderson.mourato@hotmail.com>
2024-05-31 23:36:51 -0300

    Update Main.java

--- SistemaLanchonete/src/Main.java
+++ SistemaLanchonete/src/Main.java
@@ -3,11 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Fernanda");
-
-        System.out.println("Fernanda");
-        System.out.println("Fernanda");
-        System.out.println("Fernanda");
+        
 
         System.out.println("Teste");
     }
